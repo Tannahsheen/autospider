10641fb0a6cdd8e94869861d20d801117e78295e
Tannahsheen <152140112+Tannahsheen@users.noreply.github.com>
2024-06-03 16:07:41 -0500

    Update autospider2.sh

--- autospider2.sh
+++ autospider2.sh
@@ -5,7 +5,7 @@ IP_FILE="spider-hosts.txt"
 LOOT_DIR="$HOME/manspider_loot"
 
 FILE_EXTENSIONS="txt doc docx xls xlsx csv db bat com vbs ps1 psd1 psm1 pem key rsa pub reg cfg conf"
-KEYWORDS="passw user admin account network login logon cred"
+KEYWORDS="passw user admin account network login logon api_key access_key token cred SSN passport social_security drivers_license creditcard credit_card certificate pem ssh vpn classified confidential internal_use_only"
 
 mkdir -p "$LOOT_DIR"
 
