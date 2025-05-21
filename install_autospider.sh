d1be54c2245733bca8fee9518646caae70a3798d
Tannahsheen <152140112+Tannahsheen@users.noreply.github.com>
2025-05-21 12:08:41 -0500

    Update install_autospider.sh
    changed cme to nxc

--- install_autospider.sh
+++ install_autospider.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y crackmapexec
+#sudo apt-get install -y crackmapexec
 sudo apt-get install -y smbclient
 sudo apt install -y pipx
 python3 -m pip install --user pipx
 python3 -m pipx ensurepath
+pipx install git+https://github.com/Pennyw0rth/NetExec
 pipx install git+https://github.com/blacklanternsecurity/MANSPIDER
 cd "$(dirname "$0")"
 chmod +x autospider.sh
